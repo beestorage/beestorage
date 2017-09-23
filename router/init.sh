22eaae8cced7c20dad4e2113f83b8e99a8cb27e6
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-09-23 14:23:07 +0700

    Finish 1 not work shardding

--- router/init.sh
+++ router/init.sh
@@ -1,7 +1,6 @@
 #sleep 5
-su --command "mongo --host config1 --port 27019 < /data/initdb_configserver"
+su --command "mongo --host config1 --port 27019 < /data/initdb_configserver" &
 sleep 5
 su --command "mongos --configdb configserver/config1:27019,config2:27019 --port 27081" &
 sleep 10
-#su --command "mongo --host router1 --port 27081 < /data/initdb_indexkey" &
-/bin/bash
+su --command "mongo --host router1 --port 27081 < /data/initdb_indexkey" &
