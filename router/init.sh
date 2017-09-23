e580cee897224f48faa4f3cc83e4123154c67e83
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-09-23 16:27:10 +0700

    Shard work but not stable test by jmeter dead 1040 image
    doing repp

--- router/init.sh
+++ router/init.sh
@@ -1,6 +1,6 @@
 #sleep 5
-su --command "mongo --host config1 --port 27019 < /data/initdb_configserver" &
+su --command "mongo --quiet --host config1 --port 27019 < /data/initdb_configserver" &
 sleep 5
-su --command "mongos --configdb configserver/config1:27019,config2:27019 --port 27081" &
-sleep 10
-su --command "mongo --host router1 --port 27081 < /data/initdb_indexkey" &
+su --command "mongos --quiet --configdb configserver/config1:27019,config2:27019 --port 27081" &
+sleep 20
+su --command "mongo --quiet --host router1 --port 27081 < /data/initdb_indexkey" &
