b09f575688c53cbfce168d703594a6998a4b78af
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-09-23 21:57:55 +0700

    test remove CRLF to LF
    Bug New line

--- router/init.sh
+++ router/init.sh
@@ -1,8 +1,8 @@
 #sleep 5
-su --command "mongo --host config1 --port 27019 < /data/initdb_configserver" &
+su --command "mongo --host config1 --port 27019 < /data/initdb_configserver.json" &
 su --command "mongo --host shard1n1 --port 27020 < /data/initdb_shard1nxxx.json" &
 su --command "mongo --host shard2n1 --port 27020 < /data/initdb_shard2nxxx.json" &
 sleep 20
 su --command "mongos --configdb configserver/config1:27019,config2:27019 --port 27081" &
 sleep 20
-su --command "mongo --host router1 --port 27081 < /data/initdb_indexkey" &
+su --command "mongo --host router1 --port 27081 < /data/initdb_indexkey.json" &
