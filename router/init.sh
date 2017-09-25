83001c6124649774f098739156f9fa752a141c98
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-09-25 17:48:58 +0700

    init Test1

--- router/init.sh
+++ router/init.sh
@@ -2,7 +2,8 @@
 su --command "mongo --host config1 --port 27019 < /data/initdb_configserver.json" &
 su --command "mongo --host shard1n1 --port 27020 < /data/initdb_shard1nxxx.json" &
 su --command "mongo --host shard2n1 --port 27020 < /data/initdb_shard2nxxx.json" &
+su --command "mongo --host shard3n1 --port 27020 < /data/initdb_shard3nxxx.json" &
 sleep 20
-su --command "mongos --configdb configserver/config1:27019,config2:27019 --port 27081" &
+su --command "mongos --configdb configserver/shard1n1:27019,shard1n2:27019,shard2n1:27019,shard2n2:27019,shard3n1:27019,shard3n2:27019 --port 27081" &
 sleep 20
 su --command "mongo --host router1 --port 27081 < /data/initdb_indexkey.json" &
