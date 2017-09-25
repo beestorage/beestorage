778c0178978b5a0b266a20ce614addf7af719869
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-09-25 21:48:03 +0700

    Auto script init..... Build and wait 40 sec and localhost:7000

--- router/init.sh
+++ router/init.sh
@@ -3,7 +3,8 @@ su --command "mongo --host shard1n1 --port 27019 < /data/initdb_configserver.jso
 su --command "mongo --host shard1n1 --port 27020 < /data/initdb_shard1nxxx.json" &
 su --command "mongo --host shard2n1 --port 27020 < /data/initdb_shard2nxxx.json" &
 su --command "mongo --host shard3n1 --port 27020 < /data/initdb_shard3nxxx.json" &
-sleep 20
-su --command "mongos --configdb configserver/shard1n1:27019,shard1n2:27019,shard2n1:27019,shard2n2:27019,shard3n1:27019,shard3n2:27019 --port 27081" &
-sleep 10
+sleep 40
+#su --command "mongos --configdb configserver/shard1n1:27019,shard1n2:27019,shard2n1:27019,shard2n2:27019,shard3n1:27019,shard3n2:27019 --port 27081" &
+#sleep 10
 su --command "mongo --host router1 --port 27081 < /data/initdb_indexkey.json" &
+#exec "$@"
