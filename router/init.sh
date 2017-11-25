3670255573981bff2fd38c3311c2508248eb8d11
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-11-25 15:35:01 +0700

    easy config

--- router/init.sh
+++ router/init.sh
@@ -1,7 +1,7 @@
 #sleep 5
 su --command "mongo --host config1 --port 27019 < /data/initdb_configserver.json" &
 su --command "mongo --host shard1n1 --port 27020 < /data/initdb_shard1nxxx.json" &
-su --command "mongo --host shard2n1 --port 27020 < /data/initdb_shard2nxxx.json" &
+#su --command "mongo --host shard2n1 --port 27020 < /data/initdb_shard2nxxx.json" &
 #sleep 20
 ##su --command "mongos --configdb configserver/config1:27019,config2:27019 --port 27081" &
 sleep 40
